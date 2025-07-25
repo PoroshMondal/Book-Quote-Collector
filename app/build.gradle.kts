183d2297d6ebbb4f4be210a0cc4e4628c05bc138
Porosh Mondal <poroshmondal@gmail.com>
2025-07-25 16:04:11 +0600

    necessary dependency added
    Necessary dependency added
    - room database
    - view model
    - multidex
    - dependency injection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.daggerHilt)
+    alias(libs.plugins.safeArgsPlugin)
 }
 
 android {
@@ -34,9 +36,33 @@ android {
 dependencies {
 
     implementation(libs.appcompat)
-    implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.material)
+
+    // view model
+    implementation(libs.lifecycle.viewmodel)
+    implementation(libs.lifecycle.livedata)
+
+    // room
+    implementation(libs.room.runtime)
+    annotationProcessor(libs.room.compiler)
+
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+
+    // data store preference
+    implementation(libs.datastore.preferences)
+
+    // multidex
+    implementation(libs.multidex)
+    implementation(libs.work.rxjava2)
+
+    // dependency injection
+    implementation(libs.hilt.android)
+    annotationProcessor(libs.hilt.android.compiler)
+    annotationProcessor(libs.androidx.annotation)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
