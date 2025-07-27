99ee42a6cce31b898bb0f9c0fe5b8a22ce34a22e
Porosh Mondal <poroshmondal@gmail.com>
2025-07-27 17:20:56 +0600

    added code to fetch data from db on QuoteAdd screen

--- app/src/main/java/com/bqc/somvob/bookquotecollector/ui/QuoteAdd.java
+++ app/src/main/java/com/bqc/somvob/bookquotecollector/ui/QuoteAdd.java
@@ -2,13 +2,18 @@ package com.bqc.somvob.bookquotecollector.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bqc.somvob.bookquotecollector.databinding.FragmentQuoteAddBinding;
+import com.bqc.somvob.bookquotecollector.entities.Quotes;
+import com.bqc.somvob.bookquotecollector.viewModels.QuoteViewModel;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -16,6 +21,7 @@ import dagger.hilt.android.AndroidEntryPoint;
 public class QuoteAdd extends Fragment {
 
     private FragmentQuoteAddBinding binding;
+    private QuoteViewModel quoteViewModel;
 
     public QuoteAdd() {
         // Required empty public constructor
@@ -32,4 +38,33 @@ public class QuoteAdd extends Fragment {
         binding = FragmentQuoteAddBinding.inflate(inflater,container,false);
         return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        quoteViewModel = new ViewModelProvider(requireActivity()).get(QuoteViewModel.class);
+
+        binding.btnAdd.setOnClickListener(v -> {
+            if (validate()){
+                String title = binding.etTitle.text.toString();
+                String author = binding.etAuthor.text.toString();
+                String quote = binding.etQuote.text.toString();
+                String category = binding.etQuoteCategory.text.toString();
+
+                Quotes quotes = new Quotes(1,title,author,quote,category);
+                quoteViewModel.insertQuote(quotes);
+            }
+        });
+
+    }
+
+    private boolean validate(){
+        if (binding.etQuote.text.toString().empty()){
+            binding.layoutQuote.setError("Required!");
+            return false;
+        }
+
+        return true;
+    }
+
 }
